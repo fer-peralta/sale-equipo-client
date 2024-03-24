07e08ffcb129520c4cefd3a4afa6d7dd7618d440
Fernando Peralta <fernandoemanuelperalta@gmail.com>
2024-03-24 11:00:01 -0300

    added list of players and validation form

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+
+import { Player } from "./player.interface";
 
 export default function Home() {
+
+  const [playerForm, setPlayerForm] = useState({
+    id: 0,
+    name: "",
+    surname: "",
+    attack: 0,
+    defense: 0,
+    speed: 0,
+    strength: 0,
+    stamina: 0,
+    technique: 0,
+    offensive: 0,
+    defensive: 0,
+    general: 0
+  })
+
+  const [players, setPlayers] = useState<Player[]>([])
+  const [playerId, setPlayerId] = useState(0)
+
+  const formValidation = (field: keyof typeof playerForm) => {
+    const fieldValue = playerForm[field]
+    if (typeof fieldValue !== 'number' || fieldValue < 1 || fieldValue > 10 || fieldValue === null || fieldValue === undefined) {
+      throw new Error("Completa todos los campos requeridos");
+    }
+    return true
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      formValidation('name');
+      formValidation('surname');
+      formValidation('attack');
+      formValidation('defense');
+      formValidation('speed');
+      formValidation('strength');
+      formValidation('stamina');
+      formValidation('technique');
+
+      handleAddPlayer();
+    } catch (error: any) {
+      alert(error.message);
+    }
+
+  }
+
+  const handleAddPlayer = () => {
+    const newPlayerId = playerId + 1
+    const playerOff = (playerForm.attack + playerForm.speed + playerForm.technique + playerForm.stamina) / 4
+    const playerDef = (playerForm.defensive + playerForm.strength + playerForm.speed + playerForm.stamina) / 4
+    const playerGen = (playerForm.defensive + playerForm.attack + playerForm.technique + playerForm.strength + playerForm.speed + playerForm.stamina) / 6
+    setPlayerId(newPlayerId)
+    const newPlayer = {
+      ...playerForm,
+      offensive: playerOff,
+      defensive: playerDef,
+      general: playerGen,
+      id: newPlayerId
+    }
+    setPlayers(prevPlayers => [...prevPlayers, newPlayer])
+    setPlayerForm({
+      id: 0,
+      name: "",
+      surname: "",
+      attack: 0,
+      defense: 0,
+      speed: 0,
+      strength: 0,
+      stamina: 0,
+      technique: 0,
+      offensive: 0,
+      defensive: 0,
+      general: 0
+    })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <section className="players-dashboard">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nombre<span>*</span></label>
+          <input type="text" id="name" minLength={2} maxLength={30} onChange={e => setPlayerForm({ ...playerForm, name: e.target.value })} />
+          <label htmlFor="surname">Apellido<span>*</span></label>
+          <input type="text" id="surname" minLength={2} maxLength={30} onChange={e => setPlayerForm({ ...playerForm, surname: e.target.value })} />
+          <label htmlFor="attack">Ataque<span>*</span></label>
+          <input type="number" name="attack" min={1} max={10} onChange={e => setPlayerForm({ ...playerForm, attack: parseInt(e.target.value) })} />
+          <label htmlFor="defense">Defensa<span>*</span></label>
+          <input type="number" name="defense" min={1} max={10} onChange={e => setPlayerForm({ ...playerForm, defense: parseInt(e.target.value) })} />
+          <label htmlFor="speed">Velocidad<span>*</span></label>
+          <input type="number" name="speed" min={1} max={10} onChange={e => setPlayerForm({ ...playerForm, speed: parseInt(e.target.value) })} />
+          <label htmlFor="strength">Fuerza<span>*</span></label>
+          <input type="number" name="strength" min={1} max={10} onChange={e => setPlayerForm({ ...playerForm, strength: parseInt(e.target.value) })} />
+          <label htmlFor="stamina">Resistencia<span>*</span></label>
+          <input type="number" name="stamina" min={1} max={10} onChange={e => setPlayerForm({ ...playerForm, stamina: parseInt(e.target.value) })} />
+          <label htmlFor="technique">Técnica<span>*</span></label>
+          <input type="number" name="technique" min={1} max={10} onChange={e => setPlayerForm({ ...playerForm, technique: parseInt(e.target.value) })} />
+          <input type="submit" value={"Agregar jugador"} />
+        </form>
+        {
+          players.length > 0 ?
+            <table>
+              <tbody>
+                <tr>
+                  <th>id</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Ofensiva</th>
+                  <th>Defensiva</th>
+                  <th>General</th>
+                  <th>Acciones</th>
+                </tr>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                {players.map(player => (
+                  <tr key={player.id}>
+                    <td>{player.id}</td>
+                    <td>{player.name}</td>
+                    <td>{player.surname}</td>
+                    <td>{player.offensive}</td>
+                    <td>{player.defensive}</td>
+                    <td>{player.general}</td>
+                    <td>Editar</td>
+                    <td>Borrar</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            : <p>No hay jugadores</p>
+        }
+      </section>
     </main>
-  );
+  )
 }
---
+++ app/player.interface.tsx
@@ -0,0 +1,14 @@
+export interface Player {
+  id: number;
+  name: string;
+  surname: string;
+  attack: number;
+  defense: number;
+  speed: number;
+  strength: number;
+  stamina: number;
+  technique: number;
+  offensive: number;
+  defensive: number;
+  general: number;
+}
